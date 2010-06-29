b7c12eead0b32d088de12499b5a8e019356f3873
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-06-29 20:50:26 +0000

    Nothing really.

--- getOmxs30Data.R
+++ getOmxs30Data.R
@@ -1,4 +1,3 @@
-require(fSeries)
 require(fImport)
 
 omxs30Symbols<-c('ABB.ST', 'ALFA.ST', 'ASSA-B.ST', 'AZN.ST', 'ATCO-A.ST', 'ATCO-B.ST', 'BOL.ST', 'ELUX-B.ST', 'ERIC-B.ST', 'GETI-B.ST', 'HMB.ST', 'INVE-B.ST', 'LUPE.ST', 'MTG-B.ST', 'NOKI-SEK.ST', 'NDA-SEK.ST', 'SAND.ST', 'SCA-B.ST', 'SCV-B.ST', 'SEB-A.ST', 'SECU-B.ST', 'SKA-B.ST', 'SKF-B.ST', 'SSAB-A.ST', 'SHB-A.ST', 'SWED-A.ST', 'SWMA.ST', 'TEL2-B.ST', 'TLSN.ST', 'VOLV-B.ST')
