f3a5b1659bab006bf7feb09a1779c93a9e418fda
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-07-28 11:24:32 +0000

    Now fully parsing the reuters analyst opinion.

--- parseRecommendations.pl
+++ parseRecommendations.pl
@@ -3,7 +3,10 @@
 use strict;
 use LWP::Simple;
 
-my $symbol = "ERICB.ST";
+# Error checking
+die "Usage: parseRecommendations.pl ERICB.ST\n" if(scalar @ARGV < 1);
+my $symbol = shift @ARGV;
+
 # Get the recommendation
 my $foutname = "tmp.html";
 my $content = get("http://www.reuters.com/finance/stocks/analyst?symbol=$symbol");
@@ -18,11 +21,27 @@ require HTML::FormatText;
 my $formatter = HTML::FormatText->new(leftmargin => 0, rightmargin => 50);
 $content = $formatter->format($tree);
 
+# Clean up
+unlink($foutname);
+
+# Error checking for symbol not found
+die "Error: Unknown symbol '$symbol', please provide a valid one.\n" if($content =~ /Your search for "$symbol" did not match/);
+
 # Cut away unnecessary text
 $content =~ s/^.+Analyst\sRecommendations\sand\sRevisions(.+)Consensus\sEstimates\sAnalysis.+/$1/s;
 $content =~ s/^.+(\(1\)\sBUY)/$1/s;
-$content =~ s/^.$//s;
-chomp($content);
-print $content;
+$content =~ s/\n+/\n/g;
 
+# Convert text to tokens
+my @tokens = split("\n", $content);
+
+# Convert recommendations to csv format
+while(scalar @tokens > 0)
+{
+	print shift @tokens;
+	for(my $i=0; $i<4; ++$i){
+		print ";", shift @tokens;
+	}
+	print "\n";
+}
 
