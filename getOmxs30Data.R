25dd037698ea172aac2489b80da5e2bc2c7fb66f
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-08-08 18:29:09 +0000

    Fixed a bug in the data fetcher.

--- getOmxs30Data.R
+++ getOmxs30Data.R
@@ -2,18 +2,26 @@ require(fImport)
 
 omxs30Symbols<-c('ABB.ST', 'ALFA.ST', 'ASSA-B.ST', 'AZN.ST', 'ATCO-A.ST', 'ATCO-B.ST', 'BOL.ST', 'ELUX-B.ST', 'ERIC-B.ST', 'GETI-B.ST', 'HMB.ST', 'INVE-B.ST', 'LUPE.ST', 'MTG-B.ST', 'NOKI-SEK.ST', 'NDA-SEK.ST', 'SAND.ST', 'SCA-B.ST', 'SCV-B.ST', 'SEB-A.ST', 'SECU-B.ST', 'SKA-B.ST', 'SKF-B.ST', 'SSAB-A.ST', 'SHB-A.ST', 'SWED-A.ST', 'SWMA.ST', 'TEL2-B.ST', 'TLSN.ST', 'VOLV-B.ST')
 
-loadData<-function(stocks)
-{
-	ret<-yahooSeries(stocks, nDaysBack=3660)
-	ret
-}
 
 writeData<-function(mydat)
 {
 	write.csv(mydat, file="omxs30.csv")
 }
 
+fetchData<-function(stocks)
+{
+	myData<-c()
+	for(symbol in omxs30Symbols)
+	{
+		tmp<-yahooSeries(symbol, nDaysBack=3660)
+		myData<-cbind(myData, tmp)
+		Sys.sleep(1)
+	}
+	colnames(myData)<-gsub(".Adj.Close", ".Adjusted", colnames(myData))
+	myData
+}
+
 # Load the data
-myData<-loadData(omxs30Symbols)
+myData<-fetchData(omxs30Symbols)
 writeData(myData)
 
