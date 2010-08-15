2c2123d9ec90e946f9b7855b4b9e89283d21c85e
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-08-15 23:01:48 +0000

    Added a search for calendar effects in the data.

---
+++ trunk/calendarEffects.R
@@ -0,0 +1,22 @@
+require(fImport)
+require(fPortfolio)
+require(quantmod)
+
+source('dataImportUtility.R')
+
+mySymbols<-c('lupe.st', 'bets-b.st', 'bp', 'abb.st', 'eric-b.st', 'fing-b.st', 'msft', 'noki-sek.st', 'par-sek.st', 'prec.st', 'pve.st', 'tel2-b.st')
+
+# Load the data
+myData<-fetchData(mySymbols, 365)
+# Get the log returns
+myReturns<-returns(Cl(myData))
+# Add day of week and convert to data frame
+myDf<-as.data.frame(myReturns)
+myDf<-cbind(myDf, Day=weekdays(as.Date(rownames(myReturns))))
+myDf<-cbind(myDf, Month=months(as.Date(rownames(myReturns))))
+myDf<-cbind(myDf, Quarter=quarters(as.Date(rownames(myReturns))))
+# Calculate the average return per weekday
+dayStats<-stats::aggregate(myDf$bets.b.st.Close, list(Day=myDf$Day), mean)
+monthStats<-stats::aggregate(myDf$bets.b.st.Close, list(Month=myDf$Month), mean)
+quarterStats<-stats::aggregate(myDf$bets.b.st.Close, list(Quarter=myDf$Quarter), mean)
+
--- trunk/portfolioOptimizer.R
+++ trunk/portfolioOptimizer.R
@@ -4,7 +4,7 @@ require(quantmod)
 
 source('dataImportUtility.R')
 
-mySymbols<-c('lupe.st', 'bp', 'abb.st', 'BINV.ST', 'eric-b.st', 'fing-b.st', 'msft', 'noki-sek.st', 'par-sek.st', 'prec.st', 'pve.st', 'swed-a.st', 'tel2-b.st')
+mySymbols<-c('lupe.st', 'bets-b.st', 'bp', 'abb.st', 'eric-b.st', 'fing-b.st', 'msft', 'noki-sek.st', 'par-sek.st', 'prec.st', 'pve.st', 'tel2-b.st')
 
 # Load the data
 myData<-fetchData(mySymbols, 365)
