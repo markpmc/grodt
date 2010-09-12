58749aefda1436a49be8eb24eca8035585cd7b04
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-09-12 19:25:23 +0000

    Started working on a magical TTA.

--- generateTaSignals.R
+++ generateTaSignals.R
@@ -3,7 +3,26 @@ require(ttrTests)
 sma30100<-function(x, params, burn=0, short=FALSE)
 {
 	ret<-rep(0, length(x))
-	inds<-which(SMA(x, n=30)>SMA(x, n=100))
-	ret[inds]<-1
+	d<-100-30
+	inds<-which(SMA(x[-(1:d)], n=30)>SMA(x, n=100))
+	ret[inds+d-1]<-1
 	ret
 }
+
+magicTTR<-function(x, params, burn=0, short=FALSE)
+{
+	ret<-rep(0, length(x))
+	smaShort<-12
+	smaLong<-26
+	d<-smaLong-smaShort
+	xsmall<-x[-c(1:d)]
+	mySma<-SMA(xsmall, n=smaShort) - SMA(x, n=smaLong)
+	myMacd<-macd4(x[-c(1:(smaLong-1))])
+	myRsi<-RSI(x, 21)
+	myRsi<-myRsi[-c(1:(smaLong-1))]
+	inds<-intersect(intersect(which(mySma>0), which(myMacd>0)), which(myRsi<50))
+	ret[inds+d-1]<-1
+	ret
+}
+
+
