999ff563b7c344af6cd5116ca665cce33647ec9a
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-10-17 12:40:05 +0000

    Added a more technical signals to indicate buy or selling signals

--- trunk/testTaSignals.R
+++ trunk/testTaSignals.R
@@ -2,6 +2,14 @@ require('ttrTests')
 require('quantmod')
 require('timeSeries')
 
+plotSignal<-function(x, signal)
+{
+	plot(x, type="l")
+	abline(v=which(signal<0), col="red")
+	abline(v=which(signal>0), col="green")
+}
+
+
 source('generateTaSignals.R')
 source('dataImportUtility.R')
 
