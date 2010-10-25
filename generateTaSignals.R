e6e29c58497141f46ce3c2ce9f83de22d311a544
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-10-25 05:08:54 +0000

    Rewrote the TTR function to return positions and not just indications of buy and sell. This is the behaviour TTRTests expect.

--- generateTaSignals.R
+++ generateTaSignals.R
@@ -1,16 +1,18 @@
 require(ttrTests)
 
+# All of these TTR functions should return positions, i.e., return 1 as long as we are on a buy signal. Not just when the signal is issued.
+
 smaSignal<-function(x, params=c(30,100), burn=0, short=FALSE)
 {
 	ret<-rep(0, length(x))
 	d<-SMA(x, n=params[1])-SMA(x, n=params[2])
-	d<-ifelse(d<0,-1,1)
-	d<-diff(d)
-	ret[d<0]<--1
-	ret[d>0]<-1
+	d<-ifelse(d<0,0,1)
+	d[is.na(d)]<-0
+	ret<-d
 	ret
 }
 
+# This should maybe be positive a longer time? Perhaps all the way until we hit 50?
 rsiSignal<-function(x, params=c(21, 30, 70), burn=0, short=FALSE)
 {
 	ret<-rep(0, length(x))
@@ -18,19 +20,27 @@ rsiSignal<-function(x, params=c(21, 30, 70), burn=0, short=FALSE)
 	overBought<-params[3]
 	y<-RSI(x, params[1])
 	ret[y<overSold]<-1
-	ret[y>overBought]<--1
+	ret[y>overBought]<-0
 	ret
 }
 
 macdSignal<-function(x, params=c(12, 26, 9), burn=0, short=FALSE)
 {
 	ret<-rep(0, length(x))
-	y<-MACD(x, params[1], params[2], params[3])
+	y<-MACD(as.vector(x), params[1], params[2], params[3])
 	y<-y[, "macd"]-y[, "signal"]
-	d<-ifelse(y<0,-1,1)
-	d<-diff(d)
-	ret[d<0]<--1
-	ret[d>0]<-1
+	d<-ifelse(y<0,0,1)
+	d[is.na(d)]<-0
+	ret<-d
+	ret
+}
+
+bollingerSignal<-function(x, params=c(20, 2), burn=0, short=FALSE)
+{
+	ret<-rep(0, length(x))
+	y<-BBands(x, n=params[1], sd=params[2])
+	ret[x>y[,"up"]]<-0
+	ret[x<y[,"dn"]]<-1
 	ret
 }
 
--- testTaSignals.R
+++ testTaSignals.R
@@ -2,13 +2,13 @@ require('ttrTests')
 require('quantmod')
 require('timeSeries')
 
+# Plot the financial time series and the buy and sell signals indicated by +1 and -1 respectively
 plotSignal<-function(x, signal)
 {
-	plot(x, type="l")
+	chartSeries(x, TA=c(addMACD(30,100,15),addBBands(),addRSI(21)))
 	abline(v=which(signal<0), col="red")
 	abline(v=which(signal>0), col="green")
 }
-
 
 source('generateTaSignals.R')
 source('dataImportUtility.R')
@@ -16,4 +16,12 @@ source('dataImportUtility.R')
 mydata<-readSeries('data/omxs30_10Years.csv', sep=',')
 mydata<-removeNA(Cl(mydata))
 
-returnStats(mydata$ABB.ST.Close)
+stock<-mydata[, "ABB.ST.Close"]
+stock<-as.xts(stock)
+#plotSignal(stock, diff(rsiSignal(stock, params=c(21,30,70))))
+plotSignal(stock, diff(macdSignal(stock, params=c(30,100,15))))
+#plotSignal(stock, diff(smaSignal(stock, params=c(30,100))))
+#plotSignal(stock, diff(bollingerSignal(stock, params=c(20,2))))
+
+# Check statistical validity this function requires a vector or a timeSeries with "Close" attribute
+returnStats(as.vector(stock), ttr=bollingerSignal, params=c(20,3))
