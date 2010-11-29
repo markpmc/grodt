26e867a28bb5d255171816f9c50f2b920687a01d
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-11-29 22:57:41 +0000

    Now runs through it all, but RSI needs to be updated.

--- trunk/generateTaBuySell.R
+++ trunk/generateTaBuySell.R
@@ -11,37 +11,46 @@ mySymbols<-c("ABB.ST", "ALFA.ST", "ASSA-B.ST", "AZN.ST", "ATCO-A.ST", "ATCO-B.ST
 	"TEL2-B.ST", "TLSN.ST", "VOLV-B.ST")  
 
 # Load the data from disk
-myData<-readSeries('data/omxs30_10Years.csv', sep=',')
-myData<-removeNA(Cl(myData))
+#myData<-readSeries('data/omxs30_10Years.csv', sep=',')
+#myData<-removeNA(Cl(myData))
 
 # Load the data online
-#myData<-fetchData(mySymbols, 365)
-#myData<-removeNA(Cl(myData))
+myData<-fetchData(mySymbols, 365)
+myData<-removeNA(Cl(myData))
 #myReturns<-returns(Cl(myData))
 
 # Analyze the data
 colNames<-gsub(".Close", "", colnames(myData))
 nRows<-nrow(myData)
 nCols<-ncol(myData)
-interestingStocks<-data.frame(Symbol=factor(nCols, levels=colNames), RSI=numeric(nCols), MACD=numeric(nCols), SMA=numeric(nCols), 
-	Interesting=factor(nCols, levels=c("Yes", "No")))
+interestingStocks<-data.frame(Symbol=character(0), RSI=numeric(0), MACD=numeric(0), SMA=numeric(0), 
+	Interesting=character(0))
+buySellDf<-data.frame(Symbol=character(0), RSI=character(0), MACD=character(0), SMA=character(0))
 
 for(i in 1:nCols){
 	closePrices<-myData[, i]
 	
 	# RSI
 	rsiIndicator<-RSI(closePrices, 21)[nRows]
+	rsiBuySell<-"Sell"
+	if(rsiIndicator >= 40 && rsiIndicator <= 60) rsiBuySell<-"Neutral"
+	if(rsiIndicator <= 30) rsiBuySell<-"Buy"
 	
 	# MACD
 	macdIndicator<-MACD(as.vector(closePrices), 30, 100, 9)
 	macdIndicator<-macdIndicator[nRows, "macd"]-macdIndicator[nRows, "signal"]
+	macdBuySell<-"Sell"
+	if(macdIndicator > 0) macdBuySell<-"Buy"
 	
 	# SMA
 	smaIndicator<-SMA(closePrices, 30)[nRows]/SMA(closePrices, 100)[nRows]
+	smaBuySell<-"Sell"
+	if(smaIndicator>1) smaBuySell<-"Buy"
 	
 	interesting<-"No"
 	if(rsiIndicator <= 30 || rsiIndicator >= 70) interesting<-"Yes"
-
-	interestingStocks[i, ]<-cbind(Symbol=as.character(colNames[i]), RSI=rsiIndicator, MACD=macdIndicator, SMA=smaIndicator, Interesting=interesting)	
+	interestingStocks<-rbind(interestingStocks, 
+		data.frame(Symbol=as.character(colNames[i]), RSI=rsiIndicator, MACD=macdIndicator, SMA=smaIndicator, Interesting=interesting))
+	buySellDf<-rbind(buySellDf, data.frame(Symbol=as.character(colNames[i]), RSI=rsiBuySell, MACD=macdBuySell, SMA=smaBuySell))
 }
 
