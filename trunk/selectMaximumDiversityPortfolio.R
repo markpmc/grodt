5dee0aa3116c2a4f4326cf987bcbf56e0dc5c96e
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-12-26 11:16:28 +0000

    Added a new selection for the maximized diversity portfolio.

---
+++ trunk/selectMaximumDiversityPortfolio.R
@@ -0,0 +1,21 @@
+require(fAssets)
+
+source('dataImportUtility.R')
+
+mySymbols<-scan('SymbolsYahoo.csv', what=character())
+
+# Load the data from disk
+#myData<-readSeries('data/omxs30_10Years.csv', sep=',')
+
+# Load the data online
+#myData<-fetchData(mySymbols, 365)
+
+# Use only Close prices and remove missing values
+myClose<-removeNA(myData)
+myClose<-Cl(myClose)
+
+# Build a correlation tree and arrange the securities in that order and select the top 10
+hc<-assetsSelect(myClose)
+myClose<-myClose[, assetsArrange(myClose, "hclust")]
+mySelected<-myClose[, 1:10]
+print(colnames(mySelected))
