497ae9118d194824196b115dc3ad471cfbf924a7
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2010-12-31 14:12:57 +0000

    Added liquidity risk and momentum TA indicator.

--- generateTaBuySell.R
+++ generateTaBuySell.R
@@ -1,58 +1,61 @@
-require(fPortfolio)
-require(quantmod)
+require(fTrading)
+require(TTR)
 
 source('dataImportUtility.R')
 source('taSignals.R')
 
 mySymbols<-scan('SymbolsYahoo.csv', what=character())
 
-# Load the data from disk
-#myData<-readSeries('data/omxs30_10Years.csv', sep=',')
+# Load the data
+myData<-readSeries('data/data_1year_yahoo_symbols.csv', sep=',')
+#myData<-fetchData(mySymbols, 365)
 
-# Load the data online and remove missing values
-myData<-fetchData(mySymbols, 365)
+# Remove missing values
 myData<-removeNA(myData)
 myClose<-Cl(myData)
 myVolume<-Vo(myData)
 
 # Analyze the data
-colNames<-gsub(".Close", "", colnames(myData))
-nCols<-ncol(myData)
-interestingStocks<-data.frame(Symbol=character(0), RSI=numeric(0), MACD=numeric(0), SMA=numeric(0), 
-	Volatility=numeric(0), Interesting=character(0))
-buySellDf<-data.frame(Symbol=character(0), RSI=character(0), MACD=character(0), SMA=character(0))
+colNames<-gsub(".Close", "", colnames(myClose))
+nCols<-ncol(myClose)
+stockTaData<-data.frame(Symbol=character(nCols), RSI=numeric(nCols), MACD=numeric(nCols), SMA=numeric(nCols), MOM=numeric(nCols),
+	Volatility=numeric(nCols), Liquidity=numeric(nCols), Interesting=character(nCols), stringsAsFactors=FALSE)
 nDays<-1
 for(i in 1:nCols){
-	closePrices<-myData[, i]
+	ohlcPrices<-myData[, grep(colNames[i], colnames(myData))]
+	liq<-myVolume[, grep(colNames[i], colnames(myVolume))]
+	closePrices<-myClose[, i]
 	closePrices<-removeNA(closePrices)
 	if(nrow(closePrices)==0) next
 	
 	# RSI
-	rsiIndicator<-last(RSI(closePrices, 21), nDays)
+	rsiIndicator<-last(rsiTA(closePrices, 21), nDays)
 	rsiBuySell<-"Sell"
 	if(rsiIndicator >= 40 && rsiIndicator <= 60) rsiBuySell<-"Neutral"
 	if(rsiIndicator <= 30) rsiBuySell<-"Buy"
 	
 	# MACD
-	macdIndicator<-last(MACD(as.vector(closePrices), 12, 26, 9), nDays)
-	macdIndicator<-macdIndicator["macd"]-macdIndicator["signal"]
+	macdIndicator<-last(cdoTA(closePrices, 12, 26, 9), nDays)
 	macdBuySell<-"Sell"
 	if(macdIndicator > 0) macdBuySell<-"Buy"
 	
 	# SMA
-	smaIndicator<-last(SMA(closePrices, 30), nDays)/last(SMA(closePrices, 100), nDays)
+	smaIndicator<-last(SMA(closePrices, 10) - SMA(closePrices, 25), nDays)
 	smaBuySell<-"Sell"
-	if(smaIndicator>1) smaBuySell<-"Buy"
+	if(smaIndicator>0) smaBuySell<-"Buy"
+	
+	# Momentum (ROC)
+	momIndicator<-last(rocTA(closePrices, 5), nDays)
 	
 	# Volatility
-	volIndicator<-last(TTR::volatility(closePrices, n=22), nDays)
+	volIndicator<-last(TTR::volatility(ohlcPrices, n=22), nDays)
+	
+	# Liquidity risk
+	liqRisk<-last(liq, nDays)
 	
 	interesting<-"No"
-	if(rsiIndicator <= 30 || rsiIndicator >= 70) interesting<-"Yes"
-	interestingStocks<-rbind(interestingStocks, 
-		data.frame(Symbol=as.character(colNames[i]), RSI=rsiIndicator, MACD=macdIndicator, 
-		SMA=smaIndicator, Volatility=volIndicator, Interesting=interesting))
-	buySellDf<-rbind(buySellDf, data.frame(Symbol=as.character(colNames[i]), RSI=rsiBuySell, MACD=macdBuySell, 
-		SMA=smaBuySell, Volatility=volIndicator))
+	if(rsiIndicator <= 0.30 || rsiIndicator >= 0.70) interesting<-"Yes"
+	stockTaData[i,]<-data.frame(Symbol=as.character(colNames[i]), RSI=rsiIndicator, MACD=macdIndicator, 
+		SMA=smaIndicator, MOM=momIndicator, Volatility=volIndicator, Liquidity=liqRisk, Interesting=interesting, stringsAsFactors=FALSE)
 }
 
