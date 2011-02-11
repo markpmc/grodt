3b816ef54f05b80cd6c60f07d730924ee40e9df7
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-02-11 07:27:16 +0000

    Updated the iterative prediction.

--- trunk/neuralNetPrediction.R
+++ trunk/neuralNetPrediction.R
@@ -4,21 +4,23 @@ require(nnet)
 source('dataImportUtility.R')
 
 
-1 2 3 4 5 6 7
-  2 3 4 5 6 7 8
-    3 4 5 6 7 8 9
+#1 2 3 4 5 6 7
+#  2 3 4 5 6 7 8
+#    3 4 5 6 7 8 9
  
 # predict rolling
-rollingPredict<-function(model, data, lags)
+iteratedPredict<-function(model, obs, test, lags)
 {
 	nstart<-max(lags)+1
-	data[nstart:length(data)]<-0
-	df<-data.frame(lag(data, k=lags))
-	colnames(df)<-paste("X", lags, sep="")
-	
-	for(i in nstart:length(data)){
-		df<-data.frame(lag(lags[]))
+	pred<-obs
+	pred[nstart:length(pred)]<-0
+	for(i in nstart:length(pred)){
+		mydf<-data.frame(rbind(pred[c(i-lags)]))
+		colnames(mydf)<-paste("X", lags, sep="")
+		pred[i]<-predict(model, newdata=mydf)
 	}
+	mydf<-data.frame(Target=as.numeric(obs), Predicted=as.numeric(pred))
+	mydf
 }
 
 plotPredicted<-function(df)
@@ -30,16 +32,19 @@ plotPredicted<-function(df)
 # Fetch and process data
 #myData<-fetchData('hmb.st', 180)
 myCl<-Cl(myData)
-myDataset<-data.frame(lag(myCl, k=0:12))
-colnames(myDataset)<-c("Y", paste("X", c(1:12), sep=""))
-myDataset<-myDataset[-c(1:12), ]
+myDataset<-data.frame(lag(myCl, k=0:20))
+colnames(myDataset)<-c("Y", paste("X", c(1:20), sep=""))
+myDataset<-myDataset[-c(1:20), ]
 
 
 # Build a model
 subInds<-c(1:100)
 lmfit<-lm(Y ~ ., data=myDataset, subset=subInds)
-lmfit2<-step(lmfit)
+#lmfit2<-step(lmfit)
 
 # Fitted vs. Predicted
 fitPredDf<-data.frame(Target=myDataset[subInds, 1], Predicted=predict(lmfit2))
 predSingDf<-data.frame(Target=myDataset[, 1], Predicted=predict(lmfit2, new=myDataset))
+predIteratedDf<-iteratedPredict(lmfit2, myCl, 0, c(1:20))
+
+# Neural network
