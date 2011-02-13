c83561aa1d62de8471db50f218ff34c1d02dd2e7
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-02-13 22:40:07 +0000

    Made some new improvements to the neural network.

--- trunk/neuralNetPrediction.R
+++ trunk/neuralNetPrediction.R
@@ -9,7 +9,7 @@ source('dataImportUtility.R')
 #    3 4 5 6 7 8 9
  
 # predict rolling
-iteratedPredict<-function(model, obs, test, lags)
+iteratedPredict<-function(model, obs, lags)
 {
 	nstart<-max(lags)+1
 	pred<-obs
@@ -18,7 +18,9 @@ iteratedPredict<-function(model, obs, test, lags)
 		mydf<-data.frame(rbind(pred[c(i-lags)]))
 		colnames(mydf)<-paste("X", lags, sep="")
 		pred[i]<-predict(model, newdata=mydf)
+		#browser()
 	}
+	#browser()
 	mydf<-data.frame(Target=as.numeric(obs), Predicted=as.numeric(pred))
 	mydf
 }
@@ -30,26 +32,28 @@ plotPredicted<-function(df)
 }
 
 # Fetch and process data
-#myData<-fetchData('hmb.st', 180)
+#myData<-fetchData('jm.st', 1000)
 myCl<-Cl(myData)
-myDataset<-data.frame(lag(myCl, k=0:20))
-colnames(myDataset)<-c("Y", paste("X", c(1:20), sep=""))
-myDataset<-myDataset[-c(1:20), ]
+testlags<-c(1:100)
+myDataset<-data.frame(lag(myCl, k=c(0,testlags)))
+colnames(myDataset)<-c("Y", paste("X", testlags, sep=""))
+myDataset<-myDataset[-testlags, ]
 
 
 # Build a model
-subInds<-c(1:100)
-lmfit<-lm(Y ~ ., data=myDataset, subset=subInds)
-#lmfit2<-step(lmfit)
+trainInds<-c(1:300)
+testInds<-c(1:dim(myDataset)[1])[-trainInds]
+lmfit<-lm(Y ~ ., data=myDataset, subset=trainInds)
+lmfit2<-step(lmfit)
 
 # Fitted vs. Predicted
-fitPredDf<-data.frame(Target=myDataset[subInds, 1], Predicted=predict(lmfit2))
-predSingDf<-data.frame(Target=myDataset[, 1], Predicted=predict(lmfit2, newdata=myDataset))
-predIteratedDf<-iteratedPredict(lmfit2, myCl, 0, c(1:20))
+lmFitDf<-data.frame(Target=myDataset[trainInds, 1], Predicted=predict(lmfit2))
+lmFitSingDf<-data.frame(Target=myDataset[, 1], Predicted=predict(lmfit2, newdata=myDataset))
+lmFitIteratedDf<-iteratedPredict(lmfit2, myDataset[testInds, 1], testlags)
 
 # Neural network
 myScaled<-scale(myDataset)
-nnetfit<-nnet(Y~., data=myScaled, size=20, linout=TRUE, subset=subInds, decay=0.0)
-nnFitPredDf<-data.frame(Target=as.data.frame(myScaled)[subInds, 1], Predicted=predict(nnetfit))
+nnetfit<-nnet(Y~., data=myScaled, size=5, linout=TRUE, subset=trainInds, decay=0.1)
+nnFitDf<-data.frame(Target=as.data.frame(myScaled)[trainInds, 1], Predicted=predict(nnetfit))
 nnFitSingDf<-data.frame(Target=as.data.frame(myScaled)[, 1], Predicted=predict(nnetfit, newdata=myScaled))
-predIteratedDf<-iteratedPredict(nnetfit, scale(as.data.frame(myCl)), 0, c(1:20))
+nnFitIteratedDf<-iteratedPredict(nnetfit, as.data.frame(myScaled)[testInds, 1], testlags)
