04085971b3eed2f81a8539f9b72d1574629c23a8
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-02-21 07:50:23 +0000

    Fixed a bug in the chartGenerator.

--- generateCharts.R
+++ generateCharts.R
@@ -6,7 +6,7 @@ source('dataImportUtility.R')
 generateChart<-function(stock, fname)
 {
 	png(paste(fname, ".png", sep=""))
-	chartSeries(stock, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addWMA(30), addWMA(100), addOBV(), addTDI()))
+	try(chartSeries(stock, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addWMA(30), addWMA(100), addOBV(), addTDI())))
 	dev.off()
 }
 
@@ -14,12 +14,12 @@ mySymbols<-scan('SymbolsYahoo.csv', what=character())
 
 # Load the data
 #myData<-readSeries('data/data_1year_yahoo_symbols.csv', sep=',')
-myData<-fetchData(mySymbols, 360)
+#myData<-fetchData(mySymbols, 360)
 
 for(stockName in mySymbols){
 	#browser()
 	cols<-grep(stockName, colnames(myData))
-	try(generateChart(myData[, cols], stockName))
+	generateChart(myData[, cols], stockName)
 }
 
 
