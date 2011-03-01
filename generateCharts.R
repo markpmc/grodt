dfcabe1c2e1e6f7ba3cfa0c88f3b57204484663e
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-03-01 23:54:45 +0000

    Fixed a few small bugs.

--- generateCharts.R
+++ generateCharts.R
@@ -6,7 +6,8 @@ source('dataImportUtility.R')
 generateChart<-function(stock, fname)
 {
 	png(paste(fname, ".png", sep=""), width=1280, height=600, pointsize=19)
-	try(chartSeries(stock, name=fname, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addWMA(30), addWMA(100), addOBV(), addTDI())))
+	#try(chartSeries(stock, name=fname, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addWMA(30), addWMA(100), addOBV(), addTDI())))
+	try(chartSeries(stock, name=fname, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addWMA(30), addWMA(100), addTDI(), addVo())))
 	dev.off()
 }
 
@@ -14,7 +15,7 @@ mySymbols<-scan('SymbolsYahoo.csv', what=character())
 
 # Load the data
 #myData<-readSeries('data/data_1year_yahoo_symbols.csv', sep=',')
-myData<-fetchData(mySymbols, 360)
+#myData<-fetchData(mySymbols, 360)
 
 # Delete old charts
 if(file.exists("charts")) unlink("charts", recursive=TRUE)
