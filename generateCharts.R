d55a28116d8a361dd86592f9f3c4fd29780c2fb5
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-04-18 21:06:27 +0000

    Added more stocks to the symbols and cleaned up code.

--- generateCharts.R
+++ generateCharts.R
@@ -1,28 +1,13 @@
-source('dataImportUtility.R')
-
-generateChart<-function(stock, fname)
-{
-	require(quantmod)
-	png(paste(fname, ".png", sep=""), width=1280, height=600, pointsize=19)
-	#try(chartSeries(stock, name=fname, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addWMA(30), addWMA(100), addOBV(), addTDI())))
-	try(chartSeries(stock, name=fname, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addSMA(30), addSMA(100), addVo())))
-	dev.off()
-}
-
-generateCharts<-function(stocks, mydat, path='charts')
-{
-	# Delete old charts
-	if(file.exists(path)) unlink(path, recursive=TRUE)
-	dir.create(path)
-	# Generate new charts
-	for(stockName in stocks){
-		cols<-grep(stockName, colnames(mydat))
-		generateChart(mydat[, cols], paste(path, "/", stockName, sep=""))
-	}
-}
+source('chartsGenerator.R')
+source('taScreeners.R')
 
 mySymbols<-scan('SymbolsYahoo.csv', what=character())
 myData<-fetchData(mySymbols, 360)
 #myData<-readSeries('data/data_1year_yahoo_symbols.csv', sep=',')
+
+# Generate all charts from the symbols
 generateCharts(mySymbols, myData, path="charts/")
+
+# Screen all the stocks
+myScreen<-taScreener(mySymbols, myData)
 
