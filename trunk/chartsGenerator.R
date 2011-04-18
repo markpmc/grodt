dc9e4354456b1f0999c5026e7147e69ebc8a90de
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-04-18 21:06:27 +0000

    Added more stocks to the symbols and cleaned up code.

---
+++ trunk/chartsGenerator.R
@@ -0,0 +1,23 @@
+source('dataImportUtility.R')
+
+generateChart<-function(stock, fname)
+{
+	require(quantmod)
+	png(paste(fname, ".png", sep=""), width=1280, height=600, pointsize=19)
+	#try(chartSeries(stock, name=fname, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addWMA(30), addWMA(100), addOBV(), addTDI())))
+	try(chartSeries(stock, name=fname, TA=c(addBBands(), addRSI(21), addMFI(21), addROC(10), addSMA(30), addSMA(100), addVo())))
+	dev.off()
+}
+
+generateCharts<-function(stocks, mydat, path='charts')
+{
+	# Delete old charts
+	if(file.exists(path)) unlink(path, recursive=TRUE)
+	dir.create(path)
+	# Generate new charts
+	for(stockName in stocks){
+		cols<-grep(stockName, colnames(mydat))
+		generateChart(mydat[, cols], paste(path, "/", stockName, sep=""))
+	}
+}
+
