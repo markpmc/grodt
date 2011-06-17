a2f36b5cfd6f663bf17e7c74b482a07fa422917f
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-06-17 06:17:16 +0000

    Added a xts conversion.

--- trunk/generateCharts.R
+++ trunk/generateCharts.R
@@ -2,7 +2,8 @@ source('chartsGenerator.R')
 source('taScreeners.R')
 
 mySymbols<-scan('SymbolsYahoo.csv', what=character())
-myData<-fetchData(mySymbols, 360)
+myData<-fetchData(mySymbols, 500)
+myxts<-as.xts(myData)
 #myData<-readSeries('data/data_1year_yahoo_symbols.csv', sep=',')
 
 # Generate all charts from the symbols
