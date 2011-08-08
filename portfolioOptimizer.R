006eb6ab3f952484c36ec29b4bdaa4d837a67c37
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-08-08 19:40:10 +0000

    Made some updates

--- portfolioOptimizer.R
+++ portfolioOptimizer.R
@@ -2,17 +2,31 @@ require(fPortfolio)
 require(quantmod)
 
 source('dataImportUtility.R')
+source('dataUtility.R')
+
+getRollingWeights<-function(myret, Spec, Constraints)
+{
+	a<-rollingWindows(myReturns, period="3m", by="1m")
+	b<-rollingTangencyPortfolio(myReturns, Spec, Constraints, from=a$from, to=a$to)
+	myWeights<-getWeights(b[[1]]@portfolio)
+	for(i in 2:length(b)){
+		tmp<-getWeights(b[[i]]@portfolio)
+		myWeights<-rbind(myWeights, tmp)
+	}
+	rownames(myWeights)<-as.character(a$from)
+	myWeights
+}
 
 mySymbols<-c('jm.st', 'hm.st', 'noki-sek.st', 'par-sek.st')
 mySymbols<-scan('SymbolsYahoo.csv', what=character())
 mySymbols<-scan('OMXS30SymbolsYahoo.csv', what=character())
 
 # Load the data
-myData<-fetchData(mySymbols, 365, "daily")
-myData<-Cl(myData)
-colnames(myData)<-gsub(".Close", "", colnames(myData))
-myData<-apply(myData, 2, interpolateNA)
-myData<-removeNA(myData)
+#myData<-fetchData(mySymbols, 365, "daily")
+#myData<-Cl(myData)
+#colnames(myData)<-gsub(".Close", "", colnames(myData))
+#myData<-apply(myData, 2, interpolateNA)
+#myData<-removeNA(myData)
 
 # Get the returns
 myReturns<-returns(myData)
