db31116bc77aaba790629eb9fc042cd7d8c9f0df
micke.green <micke.green@ce6ae9cc-0bbd-21b6-5c33-dae9120266b7>
2011-08-08 17:57:00 +0000

    Improving the PortfolioOptimizer.R

--- dataImportUtility.R
+++ dataImportUtility.R
@@ -17,19 +17,8 @@ fetchData<-function(stocks, ndays=365, freq="daily")
 		if(is.timeSeries(tmp)) myData<-cbind(myData, tmp)
 		Sys.sleep(1)
 	}
-	colnames(myData)<-gsub(".Adj.Close", ".Adjusted", colnames(myData))
+	myData<-fixColnames(myData)
 	myData
-}
-
-# This removes columns if all rows in that column is NA. It removes a row if any column in that row is NA.
-removeNA<-function(data)
-{
-	x<-data
-	na.col<-apply(is.na(x), 2, all)
-	if(length(na.col) > 0) x<-x[, !na.col]
-	na.row<-apply(is.na(x), 1, any)
-	if(length(na.row) > 0) x<-x[!na.row, ]
-	x
 }
 
 fetchEuroInvestorData<-function(symbols)
